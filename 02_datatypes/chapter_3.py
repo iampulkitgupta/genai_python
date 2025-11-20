4d39ceb33045e2f876c0b7d76762aa90f5acd9be
Pulkit Gupta <pulkit.noida@gmail.com>
2025-11-20 07:39:28 +0530

    feat: Add chapter 3 content demonstrating integer data types and operations.

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,36 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_tea_grams = black_tea_grams + ginger_grams
+print(total_tea_grams)
+
+remaining_tea_grams = total_tea_grams - black_tea_grams
+print(remaining_tea_grams)
+
+
+milk_litres = 7
+servings = 4
+milk_per_serving = milk_litres / servings
+print(milk_per_serving)
+
+total_tea_bags = 7
+pots = 4
+bags_per_pots = total_tea_bags // pots
+print(bags_per_pots)
+
+total_cadamom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cadamom_pods % pods_per_cup
+print(leftover_pods)
+
+
+base_flavour_strength = 2
+scale_factor = 3
+powerful_flavour = base_flavour_strength ** scale_factor
+print(powerful_flavour)
+
+
+total_tea_leaves_harvested = 1_000_000_000
+print(total_tea_leaves_harvested)
