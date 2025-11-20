1acc4829fa650c4288a76339c671cf1eb65c17ba
Pulkit Gupta <pulkit.noida@gmail.com>
2025-11-20 07:45:35 +0530

    feat: Add examples demonstrating boolean and integer data types and operations.

---
+++ 02_datatypes/chapter_4.py
@@ -0,0 +1,15 @@
+is_boiling = True
+stri_count = 5
+
+total_actions = stri_count + is_boiling
+print(total_actions)
+
+milk_present = 0
+print(bool(milk_present))
+
+
+water_hot = True
+tea_added = False
+
+can_server = water_hot and tea_added
+print(can_server)
