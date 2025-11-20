03a37f2408a5c4a2d352d4ac7347205b4da7f614
Pulkit Gupta <pulkit.noida@gmail.com>
2025-11-20 13:42:02 +0530

    feat: Add chapter 5 script demonstrating float operations and numeric type imports.

---
+++ 02_datatypes/chapter_5.py
@@ -0,0 +1,11 @@
+import sys
+from fractions import Fraction
+from decimal import Decimal as D
+
+ideal_temp = 85.5
+current_temp = 95.499999999
+
+print(f"Ideal temp {ideal_temp}")
+print(f"Current temp {current_temp}")
+print(f"Difference temp {ideal_temp - current_temp}")
+# print(sys.float_info)
